f3b2b3eb97ee07bcae2e737215e612f37623c9de
Allie <alliewritescode@gmail.com>
2025-07-28 15:35:00 +1000

    Update UserInputData.cs
    Added comments

--- Input/Touch/UserInputData.cs
+++ Input/Touch/UserInputData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "UserInputData", menuName = "Scriptable Objects/User Input Data", order = 1)]
 public class UserInputData : ScriptableObject
 {
+	/* Variable non serialized to ensure it resets on restart. This can also be 
+	 * done manually but as we don't need to view it in the inspector or save  
+	 * it long term (i.e. save to file), this should hopefully be fine. */	
     [NonSerialized]
     public Vector2 touchPos;
 }
